1bd74e02c55fa887734dcdcdb3047d428a03264d
Araceligm90 <garcia.araceli90@gmail.com>
2022-11-01 09:51:38 -0500

    second problem

--- js/app2.js
+++ js/app2.js
@@ -14,3 +14,21 @@ function sum(a, b) { //eslint-disable-line
 // Here is the test for sum(); uncomment it to run it
 sum(4, 7);
 
+
+/* Problem 2
+Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of 
+those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+
+"The product of 5 and 9 is 45."
+
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
+
+// Write your code here
+function multiply(a, b) { //eslint-disable-line
+  let answer = a * b;
+  let message = 'The product of ' + a + ' and ' + b + ' is ' + answer;
+  let result = [answer, message];
+  return result;
+}
+
+multiply(5,9);

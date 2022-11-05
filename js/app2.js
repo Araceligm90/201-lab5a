836951ae4c81e4809a9d8f7d7dc5d0d8840e7051
Araceligm90 <garcia.araceli90@gmail.com>
2022-11-05 13:40:47 -0500

    five problems resolved

--- js/app2.js
+++ js/app2.js
@@ -6,6 +6,8 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
+
+
 function sum(a, b) { //eslint-disable-line
   let twoSum = a + b;
   console.log(twoSum);
@@ -24,6 +26,7 @@ those numbers, and the second element is a string that EXACTLY follows this exam
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
+
 function multiply(a, b) { //eslint-disable-line
   let answer = a * b;
   let message = 'The product of ' + a + ' and ' + b + ' is ' + answer;
@@ -50,14 +53,26 @@ out how to do this. However, you may continue to use the + operator for string c
 testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
+
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-  let sumOfAB = sum(a, b);
-  let sumOfABC = sum(sumOfAB, c);
-  return sumOfABC;
+
+
+  let totalAdd = sum(a,b)[0];
+  let originalsum = sum(totalAdd, c)[0];
+  let totalMult = multiply(a, b)[0];
+  let originalMult = multiply(totalMult, c)[0];
+  let theMessage = `${a} and ${b} and ${c} sum to ${originalsum}.`;
+  let theSecondMessage = `The product of ${a} and ${b} and ${c} is ${originalMult}.`;
+  let arr = [];
+  arr.push( originalsum, originalMult, theMessage, theSecondMessage );
+  console.log(arr);
+  return arr;
+  
 }
+  
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-sumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 
 
@@ -76,15 +91,20 @@ Test this function by hand in the console to get it working, and when you think 
 function and see if the test passes.*/
 
 // Write your code here
+
 let testArray = [2, 3, 4]; //eslint-disable-line
 
-function sumArray(sumArr) { //eslint-disable-line
- 
+function sumArray(arr) { //eslint-disable-line
+  let firstTwoNumbs = sum(arr[0],arr[1]);
+  let additionOfthree = sum(firstTwoNumbs[0], arr[2]);
+  let ourMessage = `${arr[0]},${arr[1]},${arr[2]} was passed in as an array of numbers, and ${additionOfthree[0]} is their sum.`;
+  let returnArray =[additionOfthree[0],ourMessage];
+  return returnArray;
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 
 
@@ -103,9 +123,16 @@ Test this function by hand in the console to get it working, and when you think 
 testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
+
 function multiplyArray(multArr) { //eslint-disable-line
+  let multTwoNumbs = multiply(multArr[0],multArr[1]);
+  let multOfThree = multiply(multTwoNumbs[0],multArr[2]);
+  let multMessage = `The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${multOfThree[0]}.`;
+  let returnMultArray = [multOfThree[0],multMessage];
+  console.log(returnMultArray);
+  return returnMultArray;
 
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
